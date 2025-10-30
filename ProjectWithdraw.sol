7d994d624671410e49afac04391b457227ca5da6
Elradave <segunreal1@gmail.com>
2025-10-30 11:29:29 +0100

    Update ProjectWithdraw.sol
    First Project!
    - Created a simple contract that allows input only from the contract deployer
    - Anyone can deposit and view contract balance
    - Only deployer can withdraw into other wallets
    - Deployer can withdraw all funds into anywallet he chooses

--- ProjectWithdraw.sol
+++ ProjectWithdraw.sol
@@ -1,1 +1,40 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.14;
+
+contract SmartMoney {
+    
+    uint public received;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable{
+        received = msg.value;
+    }
+
+    function contractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function withdraw(uint _amount, address payable to) public {
+        if(msg.sender == owner){
+            to.transfer(_amount);
+        } else {
+            revert ("Error!!! You are not the owner");
+        }
+        
+    }
+
+    function withdrawAll(address payable to) public {
+        if(msg.sender == owner){
+            to.transfer(contractBalance());
+        } else {
+            revert ("Error!!! You are not the owner");
+        }
+        
+    
+    }
+}
